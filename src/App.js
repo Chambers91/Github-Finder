28bf66559af126ce2226c8caa8c1dbea512b1a3b
Terence Chambers <terencechambers91@gmail.com>
2019-10-02 19:39:40 -0400

    Alert Context API refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
 import User from './components/users/User';
@@ -7,43 +7,38 @@ import About from './components/pages/About';
 import Search from './components/users/Search';
 import Navbar from './components/layout/Navbar';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 2000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' component={User} />
-              )} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={props => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+                )} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
---
+++ src/context/alert/alertContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+//initializing file
+const alertContext = createContext();
+
+export default alertContext;
